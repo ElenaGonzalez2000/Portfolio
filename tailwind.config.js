6cd7a95e2ecdf02e5d78dc99a56915f6237101e1
Elena <elena.ig000@gmail.com>
2024-01-04 19:22:10 -0300

    :art:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/script/proyects.js", "./src/script/skills.js"],
+  content: ["./index.html", "./src/script/proyects.js", "./src/script/skills.js", "./src/script/link.js"],
   theme: {
     extend: {},
   },
